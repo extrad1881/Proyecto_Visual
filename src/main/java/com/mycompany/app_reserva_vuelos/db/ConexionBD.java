980b7a8401f6b3f0ca213f6e4adf940354558641
extrad1881 <kevinalexisjimenezgomez@gmail.com>
2025-11-21 16:15:58 -0600

    ConexionBD.java
    me dormi

--- src/main/java/com/mycompany/app_reserva_vuelos/db/ConexionBD.java
+++ src/main/java/com/mycompany/app_reserva_vuelos/db/ConexionBD.java
@@ -3,11 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.app_reserva_vuelos.db;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author tehca
  */
 public class ConexionBD {
     //Coloca aqui toda la conexion a la base de datos
+    //cambien sus usuarios por los que tengan en su mysql
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/proyectovisual?useSSL=false&serverTimezone=UTC";
+        String user = "root"; 
+        String pass = "1881";
+
+        try (Connection conn = DriverManager.getConnection(url, user, pass)) {
+            System.out.println("Conexión exitosa 🎉");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage()); 
+        }
+    }
 }
